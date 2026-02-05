d40740a7df84414c59f4a10438d8d124668c50ac
DomeneDev <domeneprodev@gmail.com>
2026-02-05 14:44:46 +0100

    Refactoriazación de main

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from inventario_logic import agregar_producto, vender_producto, obtener_resumen
+from utils import mostrar_menu, leer_cadenas, validar_opcion, validacion_dato, mostrar_venta, mostrar_resumen
 
+# ---- CONSTANTES MSG  -----
+INPUT_OPCION = "➡ Introduzca una opción: "
+INPUT_NOMBRE = "Nombre del producto: "
+INPUT_CANT = "Cantidad del producto: "
+INPUT_PREC = "Precio del producto (2 decimales): "
+
+# ---- CONSTANTES ERR ----
+ERROR_OPCION = "🛑 El valor introducido debe ser un número entero..."
+ERROR_NOMBRE = "🛑 No has introducido un nombre..."
+ERROR_PREC_NEG = "🛑 El precio no puede ser negativo..."
+ERROR_CANT_NEG = "🛑 La cantidad no puede ser negativa..."
+ERROR_DATO_INV = "ERROR: ❌ no es un valor válido, intentelo de nuevo."
 
 # Función principal de programa
+
+
 def ejecutar_inventario():
     """
     Función princpial para ejecutar el programa y su funcionalidad
@@ -10,109 +25,39 @@ def ejecutar_inventario():
     inventario = {}
     while True:
         # Mostramos menú
-        print("+---------------------------+")
-        print("| 📦Inventario app          |")
-        print("+---------------------------|")
-        print("| 1 - Agregar producto.     |")
-        print("| 2 - Venta producto.       |")
-        print("| 3 - Resumen inventario.   |")
-        print("| 4 - Salir.                |")
-        print("+---------------------------+\n")
-        # Solicitamos opción:
-        while True:
-            try:
-                opcion = int(input("➡ Introduzca una opción: \n"))
-                break
-            except ValueError:
-                print("🛑 El valor introducido debe ser un número entero.")
-        # Evaluamos opción
+        mostrar_menu()
+        # Solicitamos opción y validamos
+        opcion = validar_opcion(INPUT_OPCION, ERROR_OPCION)
         match opcion:
             case 1:
-                while True:
-                    nombre = input("Nombre del producto: ").lower()
-                    try:
-                        if not nombre.strip():
-                            raise ValueError(
-                                "🛑 No has introducido un nombre...")
-                        else:
-                            break
-                    except ValueError as e:
-                        print(f"ERROR: {e}")
-                # Validación de precio, con bucle
-                while True:
-                    precio = input("Precio del producto (2 decimales): ")
-                    try:
-                        precio = float(precio)
-                        if precio < 0:
-                            print("🛑 El precio no puede ser negativo")
-                            continue
-                        break
-                    except ValueError:
-                        print("ERROR: ❌ no es un valor válido, intentelo de nuevo")
-                # Validación de cantidad con bucle
-                while True:
-                    cantidad = input("Cantidad del producto: ")
-                    try:
-                        cantidad = int(cantidad)
-                        if cantidad < 0:
-                            print("🛑 La cantidad no puede ser negativa.")
-                            continue
-                        break
-                    except ValueError:
-                        print("ERROR: ❌ no es un valor válido, intentelo de nuevo")
-                # Llamamos a nuestra lógica de entrada
+                # Leer nombre validado
+                nombre = leer_cadenas(INPUT_NOMBRE, ERROR_NOMBRE)
+                # validar cantidad
+                cantidad = validacion_dato(
+                    INPUT_CANT, ERROR_CANT_NEG, ERROR_DATO_INV, int)
+                # Validar precio
+                precio = validacion_dato(
+                    INPUT_PREC, ERROR_PREC_NEG, ERROR_DATO_INV, float)
+                # Agregar producto
                 agregar_producto(nombre, precio, cantidad, inventario)
+                # Mensaje de confirmación.
                 print("✅ Producto añadido o modificado\n")
             case 2:
                 # Solicitamos datos del producto a vender
                 # Validación de nombre de producto
-                while True:
-                    nombre = input("Nombre del producto: ").lower()
-                    try:
-                        if not nombre.strip():
-                            raise ValueError(
-                                "🛑 No has introducido un nombre...")
-                        else:
-                            break
-                    except ValueError as e:
-                        print(f"ERROR: {e}")
+                nombre = leer_cadenas(INPUT_NOMBRE, ERROR_NOMBRE)
                 # Validación de Cantidad de venta
-                while True:
-                    cantidad = input("Cantidad del producto: ")
-                    try:
-                        cantidad = int(cantidad)
-                        if cantidad < 0:
-                            print("🛑 La cantidad no puede ser negativa.")
-                            continue
-                        break
-                    except ValueError:
-                        print("ERROR: ❌ no es un valor válido, intentelo de nuevo")
+                cantidad = validacion_dato(
+                    INPUT_CANT, ERROR_CANT_NEG, ERROR_DATO_INV, int)
                 # Comprobamos si se puede realizar la venta:
                 exito = vender_producto(nombre, cantidad, inventario)
-                if exito:
-                    print("✅Venta efectuada: ")
-                    print(f" - Nombre: {nombre}")
-                    print(f" - Cantidad: {cantidad}")
-                    print(
-                        f" - Total venta: {inventario[nombre]['precio']*cantidad} €\n")
-                else:
-                    print("📛No es posible efectura la venta: ")
-                    print("Stock insuficiente o no se encuentra producto")
-
+                # Mostra informacion de venta
+                mostrar_venta(exito, nombre, cantidad, inventario)
             case 3:
                 # Llamamamos a lógica de metricas de inventario
                 resumen = obtener_resumen(inventario)
-                print("+----------------------------------------------+")
-                print("| ✍ Resumen de inventario                     |")
-                print("+----------------------------------------------+")
-                print(
-                    f" - Total de articulos en almácen: {resumen['unidades_totales']}")
-                print(f" - Valor total del almácen: {resumen['valor_total']}")
-            case 4:
-                print("Saliendo del programa....🖐")
-                break
-            case _:
-                print("❌ Opción no válida")
+                # Mostrar resumen
+                mostrar_resumen(resumen)
 
 
 if __name__ == "__main__":
